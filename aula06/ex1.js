bd38db2fe5a9b13c8d39f1e32a9c4908bd2aa55c
Ana-Azuma <anaazuma05@gmail.com>
2025-09-10 20:36:10 -0300

    aula06
    exemplos array

---
+++ aula06/ex1.js
@@ -0,0 +1,7 @@
+// Exemplo array
+const frutas = ['Pera','Uva','Maçã']
+console.log(frutas)
+console.log(frutas[2])
+
+const frutas2 = new Array('Maçã','Morango','Laranja')
+console.log(frutas2[1]) 
---
+++ aula06/ex2array.js
@@ -0,0 +1,11 @@
+// exemplo 2 com array
+notas=[5,10,7,5];
+console.log(notas); //exibe notas
+
+// remove a ultima nota do array
+notas.pop();
+console.log(notas); 
+notas.push(9); // adiciona nota 9 no final do array
+console.log(notas);
+notas.push(10); 
+console.log(notas);
