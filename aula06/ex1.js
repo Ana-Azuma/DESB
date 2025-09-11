8c69820a358d2b871f5cbe3f541a631de7f24e1b
Ana-Azuma <anaazuma05@gmail.com>
2025-09-10 22:23:57 -0300

    aula06
    exemplos array

--- aula06/ex1.js
+++ aula06/ex1.js
@@ -1,7 +1,8 @@
 // Exemplo array
-const frutas = ['Pera','Uva','Maçã']
-console.log(frutas)
-console.log(frutas[2])
 
-const frutas2 = new Array('Maçã','Morango','Laranja')
-console.log(frutas2[1]) 
+const frutas = ['Pera','Uva','Maça']; // cria um array chamado frutas
+console.log(frutas); // exibe a lista no console
+console.log(frutas[2]); // acessa o elemento 2 do array
+
+const frutas2 = new Array('Maça','Morango','Laranja'); // segunda forma de criar array
+console.log(frutas2[1]); // acessa o indice 1 do array
---
+++ aula06/ex13obj.js
@@ -0,0 +1,12 @@
+// Exemplo classe com construtor
+
+class Pessoa{
+    // cria construtor
+    constructor(nome,idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+saudacao(){
+    return "Olá ! Meu nome "
+}
+}
